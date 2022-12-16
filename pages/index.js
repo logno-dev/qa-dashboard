6f528a11501ed11c2b91e53bdf5fe62ef9ea2c69
Logan Bunch <loganbunch@gmail.com>
2022-12-15 20:03:45 -0800

    some changes that kind of work

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import clientPromise from "../lib/mongodb";
 import Layout from '../components/layout'
 import Link from "next/link";
+import { useUser } from '@auth0/nextjs-auth0/client'
 
 export async function getServerSideProps() {
+  try {
+    const role = await fetch('https://dev-l8b7q7zqt5e0h0s0.us.auth0.com/api/v2/users/auth0%7C639b9bd718d75cfb75482fb2/permissions')
+  } catch (e) {
+    console.log(e)
+  }
   try {
     const client = await clientPromise;
     const db = client.db("products");
@@ -21,6 +27,7 @@ export async function getServerSideProps() {
 }
 
 export default function Home({ data }) {
+  const { user, error, isLoading } = useUser()
   return (
     <>
       <Layout title="Home">
@@ -28,6 +35,14 @@ export default function Home({ data }) {
           <div>
             <h1 className="text-5xl">Welcome!</h1>
             <p className="text-xl">This project is currently under construction. At this time, the batching data entry page is fully functional. Finished Product page and Reports page coming soon.</p>
+            {user ? (
+              <>
+                <p>{JSON.stringify(user)}</p>
+                <a href="/api/auth/logout" className="button m-2">Logout</a>
+              </>
+            ) : (
+              <a href="/api/auth/login" className="button m-2">Login</a>
+            )}
           </div>
         </div>
       </Layout>
